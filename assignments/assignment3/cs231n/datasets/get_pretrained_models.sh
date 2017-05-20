bcdd0ca78965050c74fc713e6a2f724dad2a914f
srikanth <srika91@gmail.com>
2017-05-20 10:57:06 +0530

    remove copy

--- assignments/assignment3/cs231n/datasets/get_pretrained_models.sh
+++
@@ -1,3 +0,0 @@
-wget http://cs231n.stanford.edu/tiny-100-A-pretrained.zip
-unzip tiny-100-A-pretrained.zip
-rm tiny-100-A-pretrained.zip
--- assignments/assignment3/cs231n/datasets/get_tiny_imagenet_splits.sh
+++
@@ -1,8 +0,0 @@
-wget http://cs231n.stanford.edu/tiny-imagenet-100-A.zip
-wget http://cs231n.stanford.edu/tiny-imagenet-100-B.zip
-
-unzip tiny-imagenet-100-A.zip
-unzip tiny-imagenet-100-B.zip
-
-rm tiny-imagenet-100-A.zip
-rm tiny-imagenet-100-B.zip
